8c52b902e9fd1a2428536d5650b258f5e417494e
Kevin McBride <kevj.m13@gmail.com>
2020-09-10 16:04:20 -0400

    fixed contact container

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,11 +6,11 @@ function Contact({ refProp }) {
         <section id='contact'>
             <div ref={refProp} className='scroll-to'></div>
             <div className='contact-grid-container'>
-                <div className='title' style={{ backgroundColor: '#83BF5F', color: '#D2ECC5' }}>
+                <div className='contact-title' style={{ backgroundColor: '#83BF5F', color: '#D2ECC5' }}>
                     <h1>Contact</h1>
                 </div>
-                <div className='content' style={{ color: '#83BF5F', backgroundColor: '#D2ECC5' }}>
-                <div><p>I'm best reached by email, but feel free to contact me through any of these other links</p></div>
+                <div className='contact-content' style={{ color: '#83BF5F', backgroundColor: '#D2ECC5' }}>
+                <div><p>I'm best reached by email, but feel free to contact me through any of these other links:</p></div>
                     <ul>
                         <li style={{ display: "flex", alignContent: "center" }}>
                         <a href="mailto:kevj.m13@gmail.com"><i className="fas fa-envelope-square fa-5x"></i></a><h1 style={{ display: 'inline', paddingLeft: '20px' }}><a href="mailto:kevj.m13@gmail.com">kevj.m13@gmail.com</a></h1>
